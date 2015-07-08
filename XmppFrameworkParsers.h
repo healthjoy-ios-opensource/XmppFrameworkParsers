07c45945c334ba1fe340ec7a82cccaeff14048e2
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-07-08 19:26:07 +0300

    Added public headers

--- XmppFrameworkParsers.h
+++ XmppFrameworkParsers.h
@@ -1,5 +1,13 @@
 
 
 
+#import <XmppFrameworkParsers/Parser/XMPPParser.h>
+#import <XmppFrameworkParsers/StanzaClasses/XMPPElement.h>
+
+#import <XmppFrameworkParsers/StanzaClasses/XMPPIQ.h>
+#import <XmppFrameworkParsers/StanzaClasses/XMPPJID.h>
+#import <XmppFrameworkParsers/StanzaClasses/XMPPMessage.h>
+#import <XmppFrameworkParsers/StanzaClasses/XMPPPresence.h>
+
 
 

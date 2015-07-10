1088f4ecdffc321bf5ced1dc6f53e23e0dcca97d
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-07-10 17:18:38 +0300

    Disabled constructors that caused crash

--- XmppFramework/Parser/XMPPParser.h
+++ XmppFramework/Parser/XMPPParser.h
@@ -4,8 +4,11 @@
 
 @interface XMPPParser : NSObject<XMPPParserProto>
 
-- (id)initWithDelegate:(id)delegate delegateQueue:(dispatch_queue_t)dq;
-- (id)initWithDelegate:(id)delegate delegateQueue:(dispatch_queue_t)dq parserQueue:(dispatch_queue_t)pq;
++ (instancetype)new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
+
+- (instancetype)initWithDelegate:(id)delegate delegateQueue:(dispatch_queue_t)dq;
+- (instancetype)initWithDelegate:(id)delegate delegateQueue:(dispatch_queue_t)dq parserQueue:(dispatch_queue_t)pq;
 
 - (void)setDelegate:(id)delegate delegateQueue:(dispatch_queue_t)delegateQueue;
 
